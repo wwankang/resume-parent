f333d1c931735cec0459565b5b987cc0752c4120
wankangldm <17611056063@163.com>
2020-05-30 19:33:07 +0800

    新增配置中心

--- resume-config/src/test/java/top/wankang/resumeconfig/ResumeConfigApplicationTests.java
+++ resume-config/src/test/java/top/wankang/resumeconfig/ResumeConfigApplicationTests.java
@@ -1,10 +1,10 @@
-package top.wankang.configserver;
+package top.wankang.resumeconfig;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ResumeConfigApplicationTests {
 
     @Test
     void contextLoads() {
